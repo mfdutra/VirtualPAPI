95ce15afed10c8d0689df40bd74a5bb912269398
Marlon Dutra <mfdutra@gmail.com>
2025-11-18 21:06:40 -0800

    remove example tests

--- VirtualPAPITests/VirtualPAPITests.swift
+++ VirtualPAPITests/VirtualPAPITests.swift
@@ -10,8 +10,4 @@ import Testing
 
 struct VirtualPAPITests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
 }
