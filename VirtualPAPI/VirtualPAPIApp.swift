5a0fc7950eefbd7204f6d6b3448d6f2ee10a7eaa
Marlon Dutra <mfdutra@gmail.com>
2025-11-21 19:11:35 -0800

    Prevent screen timeout during navigation
    Disable idle timer when app is active to keep screen on during flight
    operations. Re-enables timer when app goes to background to preserve
    battery life.

--- VirtualPAPI/VirtualPAPIApp.swift
+++ VirtualPAPI/VirtualPAPIApp.swift
@@ -8,6 +8,7 @@
 import CoreLocation
 import SwiftData
 import SwiftUI
+import UIKit
 
 @main
 struct VirtualPAPIApp: App {
@@ -17,6 +18,7 @@ struct VirtualPAPIApp: App {
     @StateObject private var gdl90Reader = GDL90Reader()
     @StateObject private var airportSelection = AirportSelection()
     @StateObject private var locationTracker = HighFrequencyLocationTracker()
+    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
@@ -39,10 +41,24 @@ struct VirtualPAPIApp: App {
 
                     // Start the initial location source listener
                     startListenerForSource(appSettings.locationSource)
+
+                    // Prevent screen from turning off
+                    UIApplication.shared.isIdleTimerDisabled = true
                 }
                 .onChange(of: appSettings.locationSource) { oldValue, newValue in
                     stopListenerForSource(oldValue)
                     startListenerForSource(newValue)
+                }
+                .onChange(of: scenePhase) { oldPhase, newPhase in
+                    // Re-enable idle timer when app goes to background to save battery
+                    switch newPhase {
+                    case .active:
+                        UIApplication.shared.isIdleTimerDisabled = true
+                    case .background, .inactive:
+                        UIApplication.shared.isIdleTimerDisabled = false
+                    @unknown default:
+                        break
+                    }
                 }
         }
     }
